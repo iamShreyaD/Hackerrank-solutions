e79384cae2dbf73fc141b00c1a5132eadecb2f75
Shreya Deshpande <70312732+iamShreyaD@users.noreply.github.com>
2025-11-22 23:08:24 +0530

    Create Day 0-Hello, World.py
    30 days of code challenge started.

---
+++ 30 Days of Code/Day 0-Hello, World.py
@@ -0,0 +1,14 @@
+Task
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.
+
+You've got this!
+
+Note: The instructions are Java-based, but we support submissions in many popular languages. You can switch languages using the drop-down menu above your editor, and the  variable may be written differently depending on the best-practice conventions of your submission language.
+
+inputString = input() # get a line of input from stdin and save it to our variable
+
+# Your first line of output goes here
+print('Hello, World.')
+
+# Write the second line of output
+print(inputString)
